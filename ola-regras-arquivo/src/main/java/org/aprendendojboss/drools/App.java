4020aaabd1a57292807409741bf8ec9eb7e7559d
William Antonio <william.fatecsjc@gmail.com>
2016-07-31 21:36:22 -0300

    Projetos iniciais

---
+++ ola-regras-arquivo/src/main/java/org/aprendendojboss/drools/App.java
@@ -0,0 +1,13 @@
+package org.aprendendojboss.drools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
